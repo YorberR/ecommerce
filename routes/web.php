51d085b949b188e6eee18d4d8ec2fbd95aff6bd2
Yorber Rojas <81208678+YorberR@users.noreply.github.com>
2025-02-18 12:22:29 -0400

    frontend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
+use App\Livewire\CartPage;
 use App\Livewire\HomePage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\ProductDetailPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get("/product/{product}", ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('my-orders', MyOrdersPage::class);
+Route::get('my-orders/{order}', MyOrderDetailPage::class);
