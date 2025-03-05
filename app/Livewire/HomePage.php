610fee05d925374aae82594d90100a566972613d
Yorber Rojas <81208678+YorberR@users.noreply.github.com>
2025-03-05 16:45:34 -0400

    comunicacion con el backend

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Home Page - Ecommerce')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        return view('livewire.home-page', [
+            'brands' => $brands
+        ]);
     }
 }
